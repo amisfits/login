8015548268e32a9928878d1c419edafef157ddcb
Misfits <48511534+amisfits@users.noreply.github.com>
2024-06-07 22:20:34 +0900

    Update in.sh

--- in.sh
+++ in.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo root:kaka666999 |sudo chpasswd root
+#!echo root:kaka666999 |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^Port 10022/Port 22/' /etc/ssh/sshd_config
