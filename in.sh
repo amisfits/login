9bf182e123eac9fae44681c0dfba12ba61671100
Misfits <48511534+amisfits@users.noreply.github.com>
2024-06-07 23:20:49 +0900

    Update in.sh

--- in.sh
+++ in.sh
@@ -3,6 +3,7 @@ echo root:kaka666999 |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^Port 10022/Port 22/' /etc/ssh/sshd_config
+sudo systemctl start firewalld
 sudo firewall-cmd --zone=public --add-port=22/tcp --permanent
 
 sudo reboot
